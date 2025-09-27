da909244391dca9bc62643b99ff3f43139dfa3dd
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2025-09-27 18:34:33 -0300

    feat (12): Create citizen home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,9 +10,14 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/mapa",
-      name: "mapa",
-      component: () => import("../views/MapaView.vue"),
+      path: '/Cidadao',
+      name: 'Cidadao',
+      component: () => import('../views/CitizenView.vue'),
+    },
+    {
+      path: '/mapa',
+      name: 'mapa',
+      component: () => import('../views/MapaView.vue'),
     },
     {
       path: "/sobre",

92531df2264af872cbc1e366742d2e2eaa33510d
BryanARM <bryanassis244@gmail.com>
2025-09-26 22:36:22 -0300

    refactor(32): corrects name for class and formats it correctly

---
+++ src/mock/types/VehicleType.ts
@@ -0,0 +1,19 @@
+export enum VehicleType {
+  CAR = "Carro",
+  PICKUP_TRUCK = "Camionete",
+  BUS = "Ônibus",
+  VAN = "Van",
+  LARGE_TRUCK = "Caminhão grande",
+  MOTORCYCLE = "Moto",
+  UNDEFINED = "Indefinido",
+}
+
+export const VehicleTypeLabel: Record<VehicleType, string> = {
+  [VehicleType.CAR]: "Carro",
+  [VehicleType.PICKUP_TRUCK]: "Camionete",
+  [VehicleType.BUS]: "Ônibus",
+  [VehicleType.VAN]: "Van",
+  [VehicleType.LARGE_TRUCK]: "Caminhão Grande",
+  [VehicleType.MOTORCYCLE]: "Moto",
+  [VehicleType.UNDEFINED]: "Indefinido",
+};

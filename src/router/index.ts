da2fbea0b6e721a103bb2e2a10fad779d8663ca5
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2025-09-28 17:16:20 -0300

    Fix(12): Fix citizen Home path

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/Cidadao",
+      path: "/cidadao",
       name: "Cidadao",
       component: () => import("../Pages/citizen/CitizenHome.vue"),
     },
